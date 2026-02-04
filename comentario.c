54c67ba7cf00721189357c9f66f14d9848cf67f2
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2026-02-04 16:04:33 +0000

    opa

---
+++ comentario.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //permite usar printf
+
+int main(void)//main() é o ponto inicial do programa
+{
+    printf("Hello, World!\n");//se remover o /n o cursor fica na mesma linha, ou seja, não pula linha
+    return 0;//o programa continua funcionando
+}
